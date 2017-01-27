9bcc80a8342a6358b6af070f05a31a1deda08e5a
Neil Thistlethwaite <neilt1111@yahoo.com>
2017-01-26 23:36:41 -0500

    start 24.py

---
+++ 24.py
@@ -0,0 +1,19 @@
+## solution for 24 game
+
+
+## operations
+	
+def mult(a,b):
+    return (a*b,str(a) + "*" + str(b))
+
+def add(a,b):
+    return (a+b,str(a) + "+" + str(b))
+
+def div(a,b):
+    return (a/b,str(a) + "/" + str(b))
+
+def sub(a,b):
+    return (a-b,str(a) + "-" + str(b))
+
+# more operations can be added if necessary
+
